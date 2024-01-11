a7ee5f8fcf1747cdaae16cdf5875372c479b1ec4
Kuvrot <jamessanz@outlook.com>
2024-01-11 17:15:13 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -8,18 +8,8 @@
 int main() {
 	
 	system ("color a");
-	system("msg * Recuerda que necesitas tener instalado Java");
-	
-	char* input = (char*) malloc (20);
-	char command[11] = "java -jar ";
-	input[0] = '\0';
-	
-	printf ("Ingrese el nombre del archivo (incluyendo la extension.jar) y presione ENTER \n (si no funciona probar con la version de 32 bits) \n");
-	scanf ("%s" , input);
-	strcat(command , input);
-	system(command);
-	
-	free(input);
+	system("msg * Recuerda que debes tener la última versión de Java instalada");
+	system ("java -jar Certifica.jar");
 	
 	return 0;
 }
